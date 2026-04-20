94739b9fa7b105bbce1d6d1d3ab73dc2c42128d9
Oceane005 <blogoce@hotmail.com>
2026-04-20 00:43:11 -0400

    feat: ajout composante philosophie - section texte

---
+++ components/philosophie/philosophie.php
@@ -0,0 +1,15 @@
+<div class="image-break image-break-2">
+  <img src="/img/sushi.png" alt="Sushis">
+  <div class="image-break-triangle"></div>
+</div>
+
+<section class="philosophie">
+  <span class="jp-title">料理哲学</span>
+  <span class="latin-title">Philosophie culinaire</span>
+  <p>
+    La cuisine japonaise est une question de respect : respect du produit, du geste et des
+    convives. Il prône une approche minimaliste où chaque élément est parfaitement exécuté
+    pour révéler le meilleur de l'ingrédient. La simplicité et la précision sont au cœur de
+    ses créations.
+  </p>
+</section>
