a39e3af443767a8726f2579be025775dc2b4fd13
Oceane005 <blogoce@hotmail.com>
2026-04-20 00:41:19 -0400

    feat: ajout composante header - HTML + CSS responsive

---
+++ components/header/header.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Iroshi Takahashi</title>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Anton&family=Barlow:wght@300;400;600&family=Noto+Serif+JP:wght@400;700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="/css/variables.css">
+  <link rel="stylesheet" href="/css/global.css">
+  <link rel="stylesheet" href="/components/header/header.css">
+  <link rel="stylesheet" href="/components/navigation/navigation.css">
+</head>
+<body>
+
+<header class="header">
+  <img src="/img/logo.png" alt="Logo Iroshi Takahashi" class="logo">
+  <button class="burger" aria-label="Menu">
+    <span></span>
+    <span></span>
+    <span></span>
+  </button>
+</header>
