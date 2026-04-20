53191ef0cde72e0bf1f6cce0e77d4b3e7323e977
Oceane005 <blogoce@hotmail.com>
2026-04-20 00:43:06 -0400

    feat: ajout composante ouverture - section grille + nuages

---
+++ components/ouverture/ouverture.php
@@ -0,0 +1,15 @@
+<div class="image-break">
+  <img src="/img/grill.png" alt="Grillades">
+  <div class="image-break-triangle"></div>
+</div>
+
+<section class="ouverture">
+  <div class="ouverture-grid"></div>
+  <img src="/img/nuage.png" alt="" class="cloud c1">
+  <img src="/img/nuage.png" alt="" class="cloud c2">
+  <img src="/img/nuage.png" alt="" class="cloud c3">
+  <div class="ouverture-text">
+    <h2 class="ouverture-line1">OUVERTURE</h2>
+    <h2 class="ouverture-line2">LE 1ER JUIN</h2>
+  </div>
+</section>
