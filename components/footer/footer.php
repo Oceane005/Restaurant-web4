58c75827362aa181c344f8056bb4b80606ae0e8e
Oceane005 <blogoce@hotmail.com>
2026-04-20 00:44:13 -0400

    feat: ajout composante footer - icones reseaux

---
+++ components/footer/footer.php
@@ -0,0 +1,13 @@
+<footer class="footer">
+  <nav class="footer-nav">
+    <a href="#"><img src="/img/tiktok-w.png"   alt="TikTok"></a>
+    <a href="#"><img src="/img/insta-w.png"    alt="Instagram"></a>
+    <a href="#"><img src="/img/facebook-w.png" alt="Facebook"></a>
+    <a href="#"><img src="/img/twitter-w.png"  alt="Twitter"></a>
+  </nav>
+</footer>
+
+<script src="/components/navigation/navigation.js"></script>
+<script src="/js/main.js"></script>
+</body>
+</html>
