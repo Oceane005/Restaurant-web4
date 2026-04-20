44f40f9410fec6147d4bb382206a8cf511bd5237
Oceane005 <blogoce@hotmail.com>
2026-04-20 00:43:34 -0400

    feat: ajout composante contact - formulaire + carte

---
+++ components/contact/contact.php
@@ -0,0 +1,14 @@
+<section class="contact" id="contact">
+  <p class="contact-label">CONTACTEZ NOUS</p>
+  <div class="contact-inner">
+    <form class="contact-form" novalidate>
+      <input type="text"  placeholder="Nom">
+      <input type="tel"   placeholder="Numéro de téléphone">
+      <input type="email" placeholder="Courriel">
+      <textarea           placeholder="Message"></textarea>
+    </form>
+    <div class="map-wrap">
+      <img src="/img/map.png" alt="Carte">
+    </div>
+  </div>
+</section>
