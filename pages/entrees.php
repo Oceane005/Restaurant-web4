b5f91ebf082844efbd8c9031fba1118adb2f1fc4
Oceane005 <blogoce@hotmail.com>
2026-04-21 21:39:07 -0400

    feat: ajout pages menu, polices, images et styles

---
+++ pages/entrees.php
@@ -0,0 +1,7 @@
+<?php require_once '../components/header.php'; ?>
+<?php require_once '../components/index.php'; ?>
+<?php require_once '../components/menu-hero.php'; ?>
+<?php require_once '../components/menu-items.php'; ?>
+<?php require_once '../components/reseaux.php'; ?>
+<?php require_once '../components/contact.php'; ?>
+<?php require_once '../components/footer.php'; ?>
